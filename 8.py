90455d026385dae9629705a6fe1cc91b42cbfd95
midoryplan <midoryfuture@gmail.com>
2026-08-08 05:49:57 +0330

    Update 8.py

--- 8.py
+++ 8.py
@@ -103,26 +103,3 @@ while True:
 
     else:
         print("only between 1 & 5!")
-
-#i_json 
-#import json 
-#import os 
-#def ij(name):
-#    if not os.path.exists(name):
-#        with open(name,"w") as file:
-#            json.dump([],file)
-
-#dict
-#from datetime import datetime
-#import hashlib
-#import socket
-#def hashing(password):
-#    hashed_password= hashlib.sha256(password.encode())
-#    return str(hashed_password.hexdigest())
-#def to_dict(name,password_hash):
-#    login_time= datetime.now()
-#    host_name= str(socket.gethostname())
-#    ip= str(socket.gethostbyname(host_name))
-#    return {"username":name,"password hash":password_hash,"login time":login_time,"host name":host_name,"ip":ip}
-
-# 98 from 100 ! 
